466aa741c24685d922e681e44575a074ab926124
isasadiqov0401~ <isays@code.edu.az>
2023-08-17 20:57:41 +0400

    Preje

---
+++ ConsolAppFinelProject/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+bool isFinished = true;
+
+while (true)
+{
+    Console.WriteLine("Proqrama xos gelmisiniz");
+    Console.WriteLine();
+    Console.WriteLine("Ana menu");
+    Console.WriteLine("1.Location emeliyyatlari");
+    Console.WriteLine("2.Restoran emeliyyatlari");
+    Console.WriteLine("3.Product  emeliyyatlari");
+    Console.WriteLine("4. Cixis");
+    Console.Write("Secin: ");
+    enterenceChoice:   string enterenceChoice = Console.ReadLine();
+    int selectedEnterenceChoice;
+    bool isTrue=int.TryParse(enterenceChoice, out selectedEnterenceChoice); 
+
+    if(isTrue)
+    {
+        switch (selectedEnterenceChoice)
+        {
+            case 1:
+                Console.WriteLine("1.Location emeliyyatlari");
+                break;
+                case 2:
+                Console.WriteLine("2.Restoran emeliyyatlari");
+                break;
+                case 3:
+                Console.WriteLine("3.Product  emeliyyatlari");
+                break;
+                case 4:
+                    isFinished= false;  
+                break;
+            default:
+                Console.WriteLine("Duzgun secin ");
+                goto enterenceChoice;
+             
+        }
+    }
+    else
+    {
+        Console.WriteLine("Duzgun secin");
+        goto enterenceChoice;
+    }
+}
---
+++ DomainLayer/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Location
+    {
+        public string Title { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+    }
+}
